fa70166a2f2184a4b28872cad14cf5c9815a5cde
Jesse <jkosowan2@academic.rrc.ca>
2022-11-22 14:11:38 -0600

    flash notice on cart create

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,8 +2,11 @@ class CartController < ApplicationController
   def create
     logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
+    product = Product.find(id)
     session[:shopping_cart] << id #push id to end of array
+    flash[:notice] = " #{product.name} added to cart."
     redirect_to root_path
+
   end
 
   def destroy
