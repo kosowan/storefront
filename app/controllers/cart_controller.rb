7e02ab77656e622ce14cd616424901197bc2dc03
Jesse <jkosowan2@academic.rrc.ca>
2022-11-22 14:26:53 -0600

    conditional display of add / remove from cart buttons

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,11 +2,13 @@ class CartController < ApplicationController
   def create
     logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
-    product = Product.find(id)
-    session[:shopping_cart] << id #push id to end of array
-    flash[:notice] = " #{product.name} added to cart."
-    redirect_to root_path
 
+    unless session[:shopping_cart].include?(id)
+      session[:shopping_cart] << id #push id to end of array
+      product = Product.find(id)
+      flash[:notice] = "➕ #{product.name} added to cart."
+    end
+    redirect_to root_path
   end
 
   def destroy
