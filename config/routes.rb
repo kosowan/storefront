54370f234f4f0915f99ba081c6e522a1c2b7f184
Jesse <jkosowan2@academic.rrc.ca>
2022-11-23 08:43:44 -0600

    modified routes file

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :categories, only: [:index, :show]
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    collection do
+      get "search"
+    end
+  end
   resources :cart, only: [:create, :destroy]
 end
