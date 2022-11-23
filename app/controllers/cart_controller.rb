5f19e78b59e8e268183497629a5860d541c2edda
Jesse <jkosowan2@academic.rrc.ca>
2022-11-23 08:25:37 -0600

    first iteration of shopping cart completed

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,7 +2,6 @@ class CartController < ApplicationController
   def create
     logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
-
     unless session[:shopping_cart].include?(id)
       session[:shopping_cart] << id #push id to end of array
       product = Product.find(id)
@@ -12,6 +11,10 @@ class CartController < ApplicationController
   end
 
   def destroy
-
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    product = Product.find(id)
+    redirect_to root_path
+    flash[:notice] = "➖ #{product.name} removed from cart."
   end
 end
