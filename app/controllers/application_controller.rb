3751e5fd2337241cc1d14ef8ed55f5c3b1e3240a
Jesse <jkosowan2@academic.rrc.ca>
2022-12-01 11:05:55 -0600

    updated new user with new fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   before_action :initialize_session
   helper_method :cart
 
+  # @provinces = Province.order(:name)
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    @provinces = Province.order(:name)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :postal_code, :address, :province_id])
+  end
 
   private
   def initialize_session
